1f6dc86805df218e56f38ef748e7af638105c3b5
Abhishek Mishra <abhishek.mishra@thrillophilia.com>
2017-04-17 01:45:53 +0530

    Comments created using props

--- src/App.js
+++ src/App.js
@@ -2,20 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class CommentBox extends Component {
+  render() {
+    const comments = this._getComments();
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="comment-box">
+              <h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
+              <div className="comment-list">
+                {comments}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  _getComments() {
+    const commentList = [
+      {
+        id: 1, author: 'Abhishek Mishra', body: 'Great Job'
+      },
+      {
+        id: 2, author: 'Vishnu Sharma', body: 'Excellent Work'
+      },
+      {
+        id: 3, author: 'Vishnu Khandal', body: 'Excellent Work'
+      }
+    ];
+    return commentList.map((comment) => {
+      return (<Comment author={comment.author} body={comment.body} key={comment.id} />);
+    });
+  }
+  _getCommentsTitle(commentCount) {
+    if (commentCount === 0) {
+      return 'No Comments Yet';
+    }
+    else if (commentCount === 1) {
+      return '1 Comment';
+    }
+    else {
+      return `${commentCount} Comments`;
+    }
+  }
+}
+
+class Comment extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="comment">
+        <p className="comment-header">{this.props.author}</p>
+        <p className="comment-body">{this.props.body}</p>
+        <div className="comment-footer">
+          <button className="btn btn-danger">Delete Comment</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default CommentBox;
